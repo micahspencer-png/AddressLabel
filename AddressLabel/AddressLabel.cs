4f5dedc8f0f60a868adc3ab849503d5f440817b3
micahspencer-png <micahspencer@isu.edu>
2025-10-17 08:48:03 -0600

    set up functions to run. Fixed bug where the button was affected

--- AddressLabel/AddressLabel.cs
+++ AddressLabel/AddressLabel.cs
@@ -8,14 +8,65 @@ namespace AddressLabel
         }
 
         void DisplayContent() 
-        { 
-        
+        {
+            if (EvaluateFields() != "") 
+            {
+                MessageBox.Show(EvaluateFields());
+            }
+
+            else 
+            {
+                DisplayText.Text = $"{FirstNametextbox.Text} {LastNametextbox.Text} \n{StreetAddresstextbox.Text} \n{Citytextbox.Text}, {Statetextbox.Text} {Zipcodetextbox.Text}";
+            }
         }
 
         string EvaluateFields() 
         {
             string message = "";
 
+            if (Zipcodetextbox.Text == "")
+            {
+                Zipcodetextbox.Focus();
+                message += "Zipcode is Required\n";
+            }
+
+            if (Statetextbox.Text == "")
+            {
+                Statetextbox.Focus();
+                message += "State is Required\n";
+            }
+
+            if (Citytextbox.Text == "")
+            {
+                Citytextbox.Focus();
+                message += "City is Required\n";
+            }
+
+            if (StreetAddresstextbox.Text == "")
+            {
+                StreetAddresstextbox.Focus();
+                message += "Street Address is Required\n";
+            }
+
+            if (LastNametextbox.Text == "")
+            {
+                LastNametextbox.Focus();
+                message += "Last Name is Required\n";
+            }
+
+            if (FirstNametextbox.Text == "")
+            {
+                FirstNametextbox.Focus();
+                message += "First Name is Required\n";
+            }
+
+            string[] messageArray;
+            messageArray = message.Split("\n");
+            message = "";
+            for (int i = messageArray.GetUpperBound(0); i >= 0; i--)
+            {
+                message += messageArray[i] + "\n";
+            }
 
             return message;
         }
@@ -27,7 +78,7 @@ namespace AddressLabel
 
         private void Clear_Click(object sender, EventArgs e)
         {
-            DisplayLabel.Text = string.Empty;
+            DisplayText.Text = string.Empty;
         }
 
         private void Exit_Click(object sender, EventArgs e)
